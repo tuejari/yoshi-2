3ec94356433c18b9f91a4c49550d5f7d02e329a9
tuejari <tuestudentjari@gmail.com>
2021-05-13 11:26:47 +0200

    Implemented PatternProcessor
    Implemented according to the thresholds from Discovering Community Patterns... by Tamburri et al. (2019)

--- src/PatternProcessor.cs
+++ src/PatternProcessor.cs
@@ -6,17 +6,26 @@ namespace YOSHI
     /// <summary>
     /// This class is responsible for transforming the numeric values of community characteristics into community 
     /// patterns.
+    /// 
+    /// The thresholds from the following paper were used:
+    /// Authors:    D.A. Tamburri, F. Palomba, A. Serebrenik, and A. Zaidman
+    /// Title:      Discovering community patterns in open - source: a systematic approach and its evaluation
+    /// Journal:    Empir.Softw.Eng.
+    /// Volume:     24
+    /// Number:     3
+    /// Pages:      1369--1417
+    /// Year:       2019
+    /// URL:        https://doi.org/10.1007/s10664-018-9659-9
     /// </summary>
     public static class PatternProcessor
     {
         // The thresholds that will be used to compute the patterns for each community.
-        private static readonly int th_geographic_distance = 4926;      // Kilometers
-        //private static readonly float th_cultural_distance = 15.0F;     // Percentages
+        private static readonly int th_global_distance = 4926;      // Kilometers
         private static readonly float th_formality_lvl_low = 0.1F;
         private static readonly float th_formality_lvl_high = 20F;
         private static readonly float th_engagement_lvl = 3.5F;
         //private static readonly float th_cohesion_lvl = 11.0F;
-        private static readonly int th_longevity = 93;                  // Days
+        private static readonly int th_longevity = 93;              // Days
 
         /// <summary>
         /// This method implements the decision tree from the YOSHI paper <see cref="Program"/>.
@@ -24,7 +33,49 @@ namespace YOSHI
         /// <param name="community">The community whose patterns should be computed.</param>
         public static void ComputePattern(Community community)
         {
-            throw new NotImplementedException();
+            Characteristics chars = community.Characteristics;
+            Pattern pattern = community.Pattern;
+
+            if (chars.Structure) // Community exhibits structure
+            {
+                pattern.SocialNetwork = true;
+
+                if (chars.Dispersion >= th_global_distance) // Dispersed
+                {
+                    pattern.NetworkOfPractice = true;
+                    if (chars.Formality < th_formality_lvl_low) // Informal
+                    {
+                        pattern.InformalNetwork = true;
+                    }
+                    else if (chars.Formality > th_formality_lvl_high) // Formal
+                    {
+                        pattern.FormalNetwork = true;
+                    }
+                }
+                else // Not dispersed
+                {
+                    pattern.CommunityOfPractice = true;
+                    //if (chars.Cohesion > th_cohesion_lvl) // Cohesive
+                    //{
+                    //    pattern.WorkGroup = true;
+                    //}
+                    if (chars.Longevity < th_longevity) // Low durability / short-lived
+                    {
+                        pattern.ProjectTeam = true;
+                    }
+                    // Note: The threshold mentions > 0.1 and < 20. Since for formality and informality we have < 0.1
+                    // and > 20 respectively, I have decided to include 0.1 and 20 in this threshold
+                    if (chars.Formality >= th_formality_lvl_low && chars.Formality <= th_formality_lvl_high) // Not informal but also not formal
+                    {
+                        pattern.FormalGroup = true;
+                    }
+                }
+
+                if (chars.Engagement > th_engagement_lvl)
+                {
+                    pattern.InformalCommunity = true;
+                }
+            }
         }
     }
 }
